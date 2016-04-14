a86947b006c57384c3e4fca8067f54f79bdde9cc
Lwin Moe <lwinmoe@gmail.com>
2016-04-14 15:53:16 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,18 @@
+# Visualizing Zipf's law in R
+
+This R script visualizes Zipf's law using a word frequency list.
+
+## Data
+
+Word counts file from http://norvig.com/ngrams/.
+
+The format of the data file is as follows:
+
+    WORD	FREQ
+    the   1000000
+    a      999999
+    ...
+
+# Ref.
+
+http://www.ccs.neu.edu/home/ekanou/ISU535.09X2/Handouts/Review_Material/zipfslaw.pdf
---
+++ zipf.R
@@ -0,0 +1,13 @@
+#!/usr/bin/Rscript
+data <- read.csv("count_1w.txt", header=TRUE,sep="\t")
+data <- data[order(data$FREQ,decreasing=TRUE),]
+order.FREQ <- order(data$FREQ,data$WORD,decreasing=TRUE)
+data$rank <- NA
+data$rank[order.FREQ] <- 1:nrow(data)
+jpeg('zipf_log.jpg')
+plot(log(data$rank), log(data$FREQ))
+dev.off()
+jpeg('zipf_raw.jpg')
+plot(data$rank, data$FREQ)
+dev.off()
+
